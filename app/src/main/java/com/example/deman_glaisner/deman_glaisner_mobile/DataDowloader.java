0889c865cf895e222c88d9448a2a925313a8da0e
julien <juliendeman.nc@gmail.com>
2016-10-27 23:10:12 +0200

    first commit - 1 button & 2 spinners

---
+++ app/src/main/java/com/example/deman_glaisner/deman_glaisner_mobile/DataDowloader.java
@@ -0,0 +1,56 @@
+package com.example.deman_glaisner.deman_glaisner_mobile;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by julo on 27/10/16.
+ */
+/*
+class DataDowloader extends AsyncTask<Void, Void, String> {
+
+
+        private Exception exception;
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+            responseView.setText("");
+        }
+
+        protected String doInBackground(Void... urls) {
+            String email = emailText.getText().toString();
+            // Do some validation here
+
+            try {
+                URL url = new URL(API_URL + "email=" + email + "&apiKey=" + API_KEY);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+            responseView.setText(response);
+        }
+    }
+
+}*/
---
+++ app/src/main/java/com/example/deman_glaisner/deman_glaisner_mobile/MainActivityFragment.java
@@ -0,0 +1,24 @@
+package com.example.deman_glaisner.deman_glaisner_mobile;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Spinner;
+import android.view.View;
+import android.widget.ArrayAdapter;
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
